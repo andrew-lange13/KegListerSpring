b88c70e6965dcc03d1b2f110bd858fd78a329f3b
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-24 12:02:50 -0500

    first commit

---
+++ src/main/java/com/codeup/keglisterspring/KegListerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.keglisterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KegListerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KegListerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/keglisterspring/KegListerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.keglisterspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KegListerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
