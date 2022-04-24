6de82210e0526b281de9134162bde577b65df99e
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-24 13:15:15 -0500

    adds user class

---
+++ src/main/java/com/codeup/keglisterspring/models/User.java
@@ -0,0 +1,17 @@
+package com.codeup.keglisterspring.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
