6db54c8078a8125a23db6e1ede55b0f7d8fca52d
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-25 16:46:48 -0500

    adds getters, setters, and constructors

--- src/main/java/com/codeup/keglisterspring/models/User.java
+++ src/main/java/com/codeup/keglisterspring/models/User.java
@@ -14,4 +14,46 @@ public class User {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public User(User copy){
+        id = copy.id;
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
 }
