d6f749c50082ab55dc6c560331f73072d6221e78
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-25 15:04:51 -0500

    name change

--- src/main/java/com/codeup/keglisterspring/repositories/PostRepository.java
+++ src/main/java/com/codeup/keglisterspring/repositories/PostRepository.java
@@ -3,5 +3,5 @@ package com.codeup.keglisterspring.repositories;
 import com.codeup.keglisterspring.models.Ad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AdRepository extends JpaRepository<Ad, Long> {
+public interface PostRepository extends JpaRepository<Ad, Long> {
 }
