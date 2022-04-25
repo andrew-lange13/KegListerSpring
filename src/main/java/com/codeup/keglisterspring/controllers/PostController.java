7bddd36f8382687fc3ff75b3c0a642b56e479f70
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-25 15:05:06 -0500

    name change

--- src/main/java/com/codeup/keglisterspring/controllers/PostController.java
+++ src/main/java/com/codeup/keglisterspring/controllers/PostController.java
@@ -1,15 +1,22 @@
 package com.codeup.keglisterspring.controllers;
 
+import com.codeup.keglisterspring.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class AdController {
+public class PostController {
+
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
 
     @GetMapping("/")
     public String posts(Model model){
-        model.addAttribute("posts", postDao.findAll());
+        model.addAttribute("ad", postDao.findAll());
         return "posts/index";
     }
 }
