66b9953c7a1b294e1e8e1af316e2e1df98c766e0
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-26 10:58:36 -0500

    adds create methods

--- src/main/java/com/codeup/keglisterspring/controllers/PostController.java
+++ src/main/java/com/codeup/keglisterspring/controllers/PostController.java
@@ -1,9 +1,16 @@
 package com.codeup.keglisterspring.controllers;
 
+import com.codeup.keglisterspring.models.Post;
+import com.codeup.keglisterspring.models.User;
 import com.codeup.keglisterspring.repositories.PostRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class PostController {
@@ -18,5 +25,23 @@ public class PostController {
     public String posts(Model model){
         model.addAttribute("post", postDao.findAll());
         return "posts/index";
+    }
+
+    @GetMapping("/create")
+    public String createPostGet(Model model){
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+
+    @PostMapping("/create")
+    public String createPost(@Valid Post post, Errors validation, Model model){
+        if (validation.hasErrors()){
+            model.addAttribute("errors", validation);
+            model.addAttribute("post", post);
+            return "posts/create";
+        }
+        post.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+        postDao.save(post);
+        return "redirect:/";
     }
 }
