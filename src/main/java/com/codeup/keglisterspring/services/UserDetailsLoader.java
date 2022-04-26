c8227d1c7bd218c97dbc7813d21da0064dab0451
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-26 10:33:19 -0500

    adds annotation

--- src/main/java/com/codeup/keglisterspring/services/UserDetailsLoader.java
+++ src/main/java/com/codeup/keglisterspring/services/UserDetailsLoader.java
@@ -6,7 +6,9 @@ import com.codeup.keglisterspring.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsLoader implements UserDetailsService {
 
     public final UserRepository users;
