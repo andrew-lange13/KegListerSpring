9d2ea3b0674ead52251fa2587d4bd4a2e2f91d21
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-26 11:26:30 -0500

    adds edit methods

--- src/main/java/com/codeup/keglisterspring/controllers/PostController.java
+++ src/main/java/com/codeup/keglisterspring/controllers/PostController.java
@@ -8,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -42,6 +44,23 @@ public class PostController {
         }
         post.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         postDao.save(post);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String getPost(@PathVariable long id, Model model){
+        model.addAttribute("post", postDao.getById(id));
+        return "/posts/edit";
+    }
+
+    @PostMapping("/{id}/edit")
+    public String editPost(@Valid@ModelAttribute Post post, Errors validation, Model model){
+        if (validation.hasErrors()){
+            model.addAttribute("errors", validation);
+            model.addAttribute("post", post);
+            return "/posts/edit";
+        }
+        post.setUser((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         return "redirect:/";
     }
 }
