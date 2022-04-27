250fcc61212893a8f0f4328daeaf38e8864c7e04
andrew-lange13 <andrew.lange13@gmail.com>
2022-04-27 10:31:58 -0500

    adds find by type

--- src/main/java/com/codeup/keglisterspring/repositories/PostRepository.java
+++ src/main/java/com/codeup/keglisterspring/repositories/PostRepository.java
@@ -2,6 +2,9 @@ package com.codeup.keglisterspring.repositories;
 
 import com.codeup.keglisterspring.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, JpaSpecificationExecutor<Post> {
+
+    Post findPostByType(String type);
 }
